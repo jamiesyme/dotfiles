f23dc59d99025e12c072e13e95dfb5afbb0fed3b
Jamie Syme <jamie.d.syme@gmail.com>
2017-01-19 17:33:02 -0800

    minfo has modules.

--- minfo/minfo-msg.py
+++ minfo/minfo-msg.py
@@ -4,9 +4,10 @@ import socket
 
 ipc_port = 6006
 
-msgs = [b'show', b'hide', b'fjaskld', b'stop']
+#msgs = [b'show', b'hide', b'fjaskld', b'stop']
+msgs = [b'stop']
 
 for msg in msgs:
-    with socket.socket() as sock:
-        sock.connect(('localhost', ipc_port))
-        sock.send(msg)
+	with socket.socket() as sock:
+		sock.connect(('localhost', ipc_port))
+		sock.send(msg)
