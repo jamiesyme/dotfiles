5632b801a92b33d672bda9769fe9f2dac1a6ea85
Jamie Syme <jamie.d.syme@gmail.com>
2017-01-30 07:45:41 -0800

    Moved minfo to its own repo.

--- minfo/src/radio-receiver.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-typedef struct Hub Hub;
-typedef struct RadioMsg RadioMsg;
-
-void runRadio(Hub* hub);
-
-void sendRadioMsg(RadioMsg msg);
--- minfo/src/radio-transmitter.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-typedef struct RadioMsg RadioMsg;
-
-void sendRadioMsg(RadioMsg msg);
