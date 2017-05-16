bde049546db52b50ebca1e84ae0227479c8b63f7
Jamie Syme <jamie.d.syme@gmail.com>
2017-05-16 07:41:28 -0700

    Updated deploy script to actually copy files instead of linking them.

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
+# Create directory structure
 mkdir -p ~/.cache \
          ~/.config \
          ~/.local/share \
          ~/bin
 
+# Copy config
 cp -R config/* ~/.config/
 ln -sf $(realpath ~/.config/bash/bashrc) ~/.bashrc
 ln -sf $(realpath ~/.config/bash/bash_profile) ~/.bash_profile
-ln -sf $(realpath scripts/init-audio.sh) ~/bin/init-audio
-ln -sf $(realpath scripts/init-feh.sh) ~/bin/init-feh
-ln -sf $(realpath scripts/init-xrandr.sh) ~/bin/init-xrandr
-ln -sf $(realpath maudio/maudio) ~/bin/maudio
-ln -sf $(realpath maudio/maudio_i3block) ~/bin/maudio_i3block
-ln -sf $(realpath minfo/minfo) ~/bin/minfo
-ln -sf $(realpath minfo/minfo-msg) ~/bin/minfo-msg
+
+# Copy bins and scripts
+cp maudio/maudio ~/bin/
+cp minfo/minfo* ~/bin/
+cp scripts/* ~/bin/
